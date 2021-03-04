5757ab5ad4bfcf8d25e632f865649a9512601771
t935175901 <58645180+t935175901@users.noreply.github.com>
2021-03-04 15:50:19 +0800

    Add files via upload

--- Twitter_spider.py
+++ Twitter_spider.py
@@ -1,22 +1,31 @@
+# coding:utf-8
+
+"""
+Twitter账号推文爬虫
+
+@Update: 2020.10.23
+"""
+
 import datetime as dt
 import re
 import time
-import json
 from urllib import parse
-from multiprocessing.dummy import Pool  # 线程池
+import json
 import crawlertool as tool
-
+from multiprocessing.dummy import Pool  # 线程池
 from selenium import webdriver
-
-
+import datetime
 class SpiderTwitterAccountPost(tool.abc.SingleSpider):
     """
     Twitter账号推文爬虫
     """
+
     def __init__(self, driver):
         self.driver = driver
+
         # 爬虫实例的变量
         self.user_name = None
+
     @staticmethod
     def get_twitter_user_name(page_url: str) -> str:
         """提取Twitter的账号用户名称
@@ -37,7 +46,6 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
         :param until_date: 抓取时间范围的左侧边界（最晚日期）
         :return: 推文信息列表
         """
-        fp = open('twitter_{}_{}_{}.json'.format(user_name, since_date, until_date), 'w', encoding='utf-8')
 
         self.user_name = user_name
 
@@ -46,7 +54,7 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
         # 生成请求的Url
         query_sentence = []
         query_sentence.append("from:%s" % user_name)  # 搜索目标用户发布的推文
-        query_sentence.append("-filter:retweets")  # 过滤到所有转推的推文
+        #query_sentence.append("-filter:retweets")  # 过滤到所有转推的推文
         if since_date is not None:
             query_sentence.append("since:%s" % str(since_date))  # 设置开始时间
             query_sentence.append("until:%s" % str(until_date))  # 设置结束时间
@@ -63,9 +71,11 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
         time.sleep(3)
 
         # 判断是否该账号在指定时间范围内没有发文
-        label_test = self.driver.find_element_by_css_selector("main > div > div > div > div:nth-child(1) >"
-                                                              " div > div:nth-child(2) > div > div")
+        #label_test = self.driver.find_element_by_css_selector("main > div > div > div > div:nth-child(1) > div > div:nth-child(2) > div > div")
+        label_test=self.driver.find_element_by_css_selector("main>div>div>div>div>div>div:nth-child(2) >div>div>div>div:nth-child(2) ")
+
         if "你输入的词没有找到任何结果" in label_test.text:
+            print("Nothing")
             return item_list
 
         # 定位标题外层标签
@@ -80,17 +90,14 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
             for label_tweet in label_outer.find_elements_by_xpath('//*[@id="outer"]/div'):  # 定位到推文标签
 
                 item = {}
-
+                try:label = label_tweet.find_element_by_css_selector(
+                    "article > div > div > div > div:nth-child(2) > div:"
+                    "nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > a")
                 # 读取推文ID
-                try:
-                    label = label_tweet.find_element_by_css_selector(
-                        "article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:n"
-                        "th-child(1) > div > div > div:nth-child(1) > a")
                 except:
                     break
-                if label:
-                    if pattern := re.search("[0-9]+$", label.get_attribute("href")):
-                        item["tweet_id"] = pattern.group()
+                if pattern := re.search("[0-9]+$", label.get_attribute("href")):
+                    item["tweet_id"] = pattern.group()
                 if "tweet_id" not in item:
                     self.log("账号名称:" + user_name + "|未找到推文ID标签(第" + str(len(item_list)) + "条推文)")
                     continue
@@ -105,7 +112,11 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
                 # 解析推文发布时间
                 if label := label_tweet.find_element_by_css_selector(
                         "article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > a > time"):
-                    item["time"] = label.get_attribute("datetime").replace("T", " ").replace(".000Z", "")
+                    timeOrg = datetime.datetime.strptime(label.get_attribute("datetime").replace("T", " ").replace(".000Z", ""), "%Y-%m-%d %H:%M:%S")
+                    deltaH = datetime.timedelta(hours=8)
+                    timeRec = timeOrg + deltaH
+                    item["time"] = timeRec.strftime("%Y-%m-%d %H:%M:%S")
+                    #消除时区偏差+8h
 
                 # 解析推文内容
                 if label := label_tweet.find_element_by_css_selector(
@@ -121,44 +132,44 @@ class SpiderTwitterAccountPost(tool.abc.SingleSpider):
                         "article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div[role='group']"):
                     if text := label.get_attribute("aria-label"):
                         # 解析推文反馈数据
-                        for feedback_item in text.split(","):
+                        for feedback_item in text.split("、"):
                             if "回复" in feedback_item:
                                 if pattern := re.search("[0-9]+", feedback_item):
                                     item["replies"] = int(pattern.group())
-                            if "转推" in feedback_item:
+                            elif "转推" in feedback_item:
                                 if pattern := re.search("[0-9]+", feedback_item):
                                     item["retweets"] = int(pattern.group())
-                            if "喜欢" in feedback_item:
+                            elif "喜欢" in feedback_item:
                                 if pattern := re.search("[0-9]+", feedback_item):
                                     item["likes"] = int(pattern.group())
 
                 item_list.append(item)
+
             # 向下滚动到最下面的一条推文
             if last_label_tweet is not None:
                 self.driver.execute_script("arguments[0].scrollIntoView();", last_label_tweet)  # 滑动到推文标签
-                self.console(user_name, "执行一次向下翻页...")
-                time.sleep(2)
+                self.console("执行一次向下翻页...")
+                time.sleep(3)
             else:
                 break
+        fp = open('twitter_{}_{}_{}.json'.format(user_name, since_date, until_date), 'w', encoding='utf-8')
         json.dump(item_list, fp=fp, ensure_ascii=False)
-        return item_list
-
-
+        print("{} is done".format(user_name))
+        #return item_list
 def run(x):
     driver = webdriver.Chrome()
     return SpiderTwitterAccountPost(driver).running(x[0], x[1], x[2])
 
-
 # ------------------- 单元测试 -------------------
 if __name__ == "__main__":
     user_names = []
-    with open("user_names.txt", "r") as fp:#读取待爬取用户用户名
+    with open("user_names.txt", "r") as fp:  # 读取待爬取用户用户名
         for line in fp:
             user_names.append(line.strip())
+    driver = webdriver.Chrome()
     num = len(user_names)
-    since_date = dt.date(2020, 11, 1)
-    until_date = dt.date(2020, 11, 15)
-    #待查询动态时间间隔
+    since_date = dt.date(2020, 10, 1)
+    until_date = dt.date(2020, 10, 7)
     combe = lambda urls_list, since_date, until_date: list(zip(urls_list, [since_date] * num, [until_date] * num))
     pool = Pool(num)
     pool.map(run, combe(user_names, since_date, until_date))
